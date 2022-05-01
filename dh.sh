9da139c9005acd86cd52c661bcd019362af98e86
pwndrer4 <53749719+pwndrer4@users.noreply.github.com>
2022-05-01 10:43:40 +0800

    Create dh.sh

---
+++ dh.sh
@@ -0,0 +1,5 @@
+curl --referer https://www.z68p.cn/ --user-agent 'Chrome/54.0 (Windows NT 10.0)' https://www.188dh.cn/
+curl --referer https://www.z68p.cn/ --user-agent 'Chrome/54.0 (Windows NT 10.0)' http://www.wxlyf.com/
+curl --referer https://z68p.cn/ --user-agent 'Chrome/54.0 (Windows NT 10.0)' http://www.wxlyf.com/
+curl --referer https://z68p.cn/ --user-agent 'Chrome/54.0 (Windows NT 10.0)' http://www.wkong.net/
+curl --referer https://www.z68p.cn/ --user-agent 'Chrome/54.0 (Windows NT 10.0)' http://www.wkong.net/
