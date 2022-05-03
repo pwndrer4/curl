a064255d7fee44c4ab92daa8e9b0e0812dc855e9
pwndrer4 <53749719+pwndrer4@users.noreply.github.com>
2022-05-03 18:53:24 +0800

    Update dh.sh

--- dh.sh
+++ dh.sh
@@ -3,3 +3,4 @@ curl --referer https://www.z68p.cn/ --user-agent 'Chrome/54.0 (Windows NT 10.0)'
 curl --referer https://www.z68p.cn/ --user-agent 'Chrome/54.0 (Windows NT 10.0)' https://www.dhduck.com/
 curl --referer https://www.z68p.cn/ --user-agent 'Chrome/54.0 (Windows NT 10.0)' https://www.wc235.com/
 curl --referer https://www.z68p.cn/ --user-agent 'Chrome/54.0 (Windows NT 10.0)' http://sh991.cn/
+curl --referer https://hblog.cf/ --user-agent 'Chrome/54.0 (Windows NT 10.0)' http://www.wxlyf.com/
